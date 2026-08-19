cc80eb533a8022b79d9ed17bf85d0c92445a92fa
CarlosPereira <carlos.pereira21ofc@gmail.com>
2026-08-18 22:27:04 -0300

    Criei um arquivo onde contém as funções do menu, e outro arquivo para chamar essas funções em um loop e se finalizar quandoi eu quiser. E eu salvei todos os dicionarios em um arquivo .json

---
+++ agenda_final.py
@@ -0,0 +1,21 @@
+import menu_agenda
+
+while True:
+    print("[1]Adicionar")
+    print("[2]Buscar")
+    print("[3]Listar")
+    print("[4]Remover")
+    print("[5]Sair")
+    opcao = input("Digite sua opção: ")
+
+    match opcao:
+        case "1": 
+            menu_agenda.adicionar()
+        case "2": 
+            menu_agenda.buscar()
+        case "3":
+            menu_agenda.listar()
+        case "4":
+            menu_agenda.remover
+        case "5":
+            break 
---
+++ menu_agenda.py
@@ -0,0 +1,58 @@
+import json
+
+#encoding="utf-8" garante que caracteres com acento sejam lidos corretamente. 
+#Sem isso, em alguns sistemas o Python pode usar uma codificação diferente e bagunçar o texto
+try:
+    #leio o "contatos.json" caso dê erro de não existir um arquivo json o código vai para a exceção
+    with open("contatos.json", "r", encoding="utf-8") as arquivo:
+        #O método .load é para ler o arquivo
+        contatos = json.load(arquivo)
+
+#Caso não exista o dicionario, a exceção criará
+except FileNotFoundError:
+    contatos = {}
+
+#Criei funções para cada função da agenda (salvar, adicionar, buscar, listar e remover)
+
+def salvar():
+    #Leitura do arquivo "contatos.json"
+    with open("contatos.json", "w", encoding="utf-8") as arquivo:
+                #adicionado o arquivo atualizado no arquivo "contatos.json" com o método .dump
+                json.dump(contatos, arquivo)
+
+def adicionar():
+    nome = input("Digite o nome: ")
+    telefone = input(f"Digite o numero do {nome}: ")
+    email = input(f"Digite o email do {nome}: ")
+
+    if nome not in contatos:
+        contatos[nome] = {"telefone": telefone, "email": email}
+        salvar()
+        return contatos[nome]
+        
+    else:
+        return "Nome já utilizado, adicione um sobrenome ou um usuario diferente"
+
+def buscar():
+    busca = input("Digite o nome que deseja buscar: ")
+    if busca in contatos:
+        return contatos[busca]
+    else:
+        return "Valor não encontrado"
+
+def listar():
+    if not contatos:
+        return "Nenhum contato cadastrado"
+    else:
+        for nome, contatos in contatos.items():
+            print(nome, contatos)
+
+def remover():
+    nome_remover = input("Digite o nome que deseja remover: ")
+    if nome_remover in contatos:
+        contatos.pop(nome_remover)#Removendo a chave desejada
+        salvar()
+
+        return f"{nome_remover} removido da agenda"
+    else:
+        return "Nome não encontrado"
